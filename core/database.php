d4c6e7cc6ef1f85f1c293fde3b2e26dba2a31c65
Rémy Martin <59732912+rmymrtin@users.noreply.github.com>
2021-01-23 23:54:30 +0100

    Update database.php

--- core/database.php
+++ core/database.php
@@ -4,17 +4,17 @@ class Database
     public static function getDB()
     {
         try {
-            $host = "localhost"; // Hôte de la base de données
-            $dbname = "deval"; // Nom de la base de données
-            $user = "root"; // Nom d'utilisateur de la base de données
-            $pass = ""; // Mot de passe de la base de données
+            $host = "localhost"; // Host
+            $dbname = "deval"; // Database name
+            $user = "root"; // Username
+            $pass = ""; // Password
 
             $db = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $user, $pass);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $db->exec("SET CHARACTER SET utf8");
             return $db;
         } catch (PDOException $e) {
-            die('Un probleme est survenu lors de la connexion a la base de donnees :');
+            die('Error :');
         }
     }
 }
